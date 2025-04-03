5dbbb1b5627c23e2bedec363601dbe62bcdc3be2
onose <ogbidionose@gmail.com>
2025-04-03 11:53:47 +0100

    add: database setup using prisma

--- actions/register.ts
+++ actions/register.ts
@@ -3,8 +3,10 @@
 import * as z from "zod";
 
 import { RegisterSchema } from "@/schemas";
+import { db } from "@/lib/db";
 
 export const register = async(values: z.infer<typeof RegisterSchema>) => {
+    
     const validatedFields = RegisterSchema.safeParse(values)
 
     if(!validatedFields.success) {
